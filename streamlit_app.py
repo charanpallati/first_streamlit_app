82739447f2fa374a3a26fead56876ab71acaf23d
Sai Charan Pallati <36511701+charanpallati@users.noreply.github.com>
2023-09-23 12:17:08 +0530

    Update streamlit_app.py
    Added dataframe to display in ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
-streamlit.header("This is header")
+import pandas as pd
+streamlit.header("Menu")
 
-streamlit.title("Hello welcome to streamlit")
+streamlit.title("Breakfast favorites")
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
