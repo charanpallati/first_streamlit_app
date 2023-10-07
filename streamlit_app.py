4ab4a93e2e7459dd43fe825484280f6a333960f6
Sai Charan Pallati <36511701+charanpallati@users.noreply.github.com>
2023-10-07 23:28:53 +0530

    added the dynamic variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,15 @@ if streamlit.button("Get Fruit load list"):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows=get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
+  my_cnx.close()
 #Allow end user to add fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-         my_cur.execute("insert into fruit_load_list  values('from streamlit')")
+         my_cur.execute("insert into fruit_load_list  values('"+ new_fruit +  "')")
          return "thanks for adding:"+ new_fruit
+streamlit.header("View our favorite list! also add your favorite list")
 add_my_fruit=streamlit.text_input("what fruit would you like to add")
+
 if streamlit.button("add a fruit to the list"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
